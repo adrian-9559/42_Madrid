75a2c8b08fa1d3c32f5d973090c44885a8c8506a
Adrián Escribano Pérez <adrian.escribano3@gmail.com>
2025-05-05 23:52:21 +0200

    Subida casi acabada de get_next_line

---
+++ Cursus/C/get_next_line/get_next_line_utils.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adriescr <adriescr@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/05 19:00:15 by adriescr          #+#    #+#             */
+/*   Updated: 2025/05/05 23:48:38 by adriescr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*dup;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	dup = malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!dup)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*joined_str;
+	int		i;
+	int		j;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1)
+		return (ft_strdup(s2));
+	if (!s2)
+		return (ft_strdup(s1));
+	joined_str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!joined_str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		joined_str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		joined_str[i++] = s2[j++];
+	joined_str[i] = '\0';
+	free(s1);
+	return (joined_str);
+}
+
+char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*joined_str;
+
+	joined_str = ft_strjoin(s1, s2);
+	if (s1)
+		free(s1);
+	return (joined_str);
+}
