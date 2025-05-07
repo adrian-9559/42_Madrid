82992959897a36ed520a96ede935e87bffa93e0f
Adrián Escribano Pérez <adrian.escribano3@gmail.com>
2025-05-07 16:39:45 +0200

    Arreglo supuestamente para lstmap

--- Cursus/C/Libft/ft_lstmap_bonus.c
+++ Cursus/C/Libft/ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: adriescr <adriescr@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 18:11:50 by adriescr          #+#    #+#             */
-/*   Updated: 2025/04/23 18:29:40 by adriescr         ###   ########.fr       */
+/*   Updated: 2025/05/07 16:36:41 by adriescr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,43 +15,51 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
-	t_list	*new_elem;
+	t_list	*new_node;
+	void	*new_content;
 
 	if (!lst || !f)
 		return (NULL);
 	new_list = NULL;
 	while (lst)
 	{
-		new_elem = (t_list *)malloc(sizeof(t_list));
-		if (!new_elem)
+		new_content = f(lst->content);
+		new_node = malloc(sizeof(t_list));
+		if (!new_node)
 		{
+			del(new_content);
 			ft_lstclear(&new_list, del);
 			return (NULL);
 		}
-		new_elem->content = f(lst->content);
-		new_elem->next = NULL;
-		ft_lstadd_back(&new_list, new_elem);
+		new_node->content = new_content;
+		new_node->next = NULL;
+		ft_lstadd_back(&new_list, new_node);
 		lst = lst->next;
 	}
 	return (new_list);
 }
 
 /*
-int main(void)
+int	main(void)
 {
 	t_list	*list;
-	t_list	*new_list;
+	t_list	*node1;
+	t_list	*node2;
+	t_list	*node3;
 
-	list = ft_lstnew("Hello");
-	ft_lstadd_back(&list, ft_lstnew("World"));
-	ft_lstadd_back(&list, ft_lstnew("42"));
-	new_list = ft_lstmap(list, &ft_strdup, &free);
-	while (new_list)
+	list = NULL;
+	node1 = ft_lstnew("Node 1");
+	node2 = ft_lstnew("Node 2");
+	node3 = ft_lstnew("Node 3");
+	ft_lstadd_back(&list, node1);
+	ft_lstadd_back(&list, node2);
+	ft_lstadd_back(&list, node3);
+	printf("Original list:\n");
+	while (list)
 	{
-		printf("%s\n", (char *)new_list->content);
-		new_list = new_list->next;
+		printf("%s\n", (char *)list->content);
+		list = list->next;
 	}
-	ft_lstclear(&list, &free);
 	return (0);
 }
 */
